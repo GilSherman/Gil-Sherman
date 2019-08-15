4c943fc3046f47ce150826b9b0cdc3c4cec73677
tal aya shefi <tal-aya.s@campus.technion.ac.il>
2019-08-15 23:17:11 +0300

    add backgound_mux basic logic

--- tachless/background_priority_mux.sv
+++ tachless/background_priority_mux.sv
@@ -12,12 +12,32 @@ module	background_priority_mux	(
 		input	logic	[7:0] BoomRGB, //Boom RGB
 		input	logic	BoomDrawingRequest,
 
-		output	logic	[7:0] backGroundRGB //the selected background
+		output logic	[7:0] backGroundRGB //the selected background
 );
 
 
 
-//fill your code here
+logic [7:0] tmpRGB;
 
+
+always_ff@(posedge clk or negedge resetN)
+begin
+	if(!resetN) begin
+			tmpRGB	<= 8'b0;
+	end
+	else begin
+		if (BoomDrawingRequest == 1'b1 )   
+			tmpRGB <= BoomRGB;  //first priority 
+		else begin
+			if (IceDrawingRequest == 1'b1 )   
+				tmpRGB <= IceRGB;  //seconed priority 
+			else begin
+				tmpRGB <= backGroundRGB ; // last priority 
+			end
+		end
+	end ; 
+end
+
+assign backGroundRGB = tmpRGB;
 
 endmodule
---
+++ tachless/background_priority_mux.sv.bak
@@ -0,0 +1,23 @@
+//background_priority_mux
+//chooses background depending on the state (normal, Ice or BOOM)
+
+module	background_priority_mux	(	
+		input	logic	clk,
+		input	logic	resetN,
+		input	logic [7:0]	BasicBackgroundRGB,  //regular backgound RGB
+		
+		input	logic	[7:0] IceRGB,  //Ice RGB
+		input	logic	IceDrawingRequest,
+		
+		input	logic	[7:0] BoomRGB, //Boom RGB
+		input	logic	BoomDrawingRequest,
+
+		output	logic	[7:0] backGroundRGB //the selected background
+);
+
+
+
+//fill your code here
+
+
+endmodule
